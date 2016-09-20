f16b8b81d3f421fe4900efb49c837711e29b99cf
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-09-20 16:46:45 +0100

    Update repo url and ignored exceptions

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -6,7 +6,7 @@ if Rails.env.staging? || Rails.env.production?
   ExceptionNotification.configure do |config|
     # Ignore additional exception types.
     # ActiveRecord::RecordNotFound, AbstractController::ActionNotFound and ActionController::RoutingError are already added.
-    # config.ignored_exceptions += %w{ActionView::TemplateError CustomError}
+    config.ignored_exceptions += %w{ActionController::InvalidAuthenticityToken I18n::InvalidLocale}
 
     # Adds a condition to decide when an exception must be ignored or not.
     # The ignore_if method can be invoked multiple times to add extra conditions.
