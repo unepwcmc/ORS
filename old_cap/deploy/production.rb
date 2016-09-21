617886fc0be935265bde8de2789681477792a175
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-09-21 14:56:31 +0100

    Remove old capistrano

--- old_cap/deploy/production.rb
+++
@@ -1,7 +0,0 @@
-set :rails_env, "production"
-
-# Primary domain name of your application. Used in the Apache configs
-set :domain, "unepwcmc-014.vm.brightbox.net"
-
-## List of servers
-server "unepwcmc-014.vm.brightbox.net", :app, :web, :db, :primary => true
