2ad2fe97114620bcd6485d0d77cd9b656a9b5cb7
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-02-22 12:28:46 +0000

    Use login field instead of email field

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   acts_as_authentic do |c|
     c.login_field = 'email'
     c.disable_perishable_token_maintenance true
-    c.validates_format_of_email_field_options = {:with => Authlogic::Regex.email_nonascii}
+    c.validates_format_of_login_field_options = { with: Authlogic::Regex.email_nonascii }
     c.merge_validates_length_of_password_field_options({:minimum => 5})
   end
 
