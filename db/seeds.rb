4d4ee37585db71bcaf99d26a0154294733584af3
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-09-07 10:54:33 +0100

    Change the db seed to avoid duplicates and include super delegate

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-admin = Role.create(:name => 'admin')
-respondent = Role.create(:name => 'respondent')
-Role.create(:name => 'delegate')
-puts "#{Role.count} role created. admin, respondent and delegate"
+admin = Role.where(name: 'admin').first_or_create
+respondent = Role.where(name: 'respondent').first_or_create
+Role.where(name: 'delegate').first_or_create
+Role.where(name: 'super_delegate').first_or_create
+puts "#{Role.count} role created. admin, respondent, delegate and super_delegate"
 user = User.create(:first_name => "My", :last_name => "Admin",
             :email => "admin@unep-wcmc.org", :password => "admin",
             :password_confirmation => "admin")
