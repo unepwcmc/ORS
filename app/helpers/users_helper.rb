9083ca4ec9073c70f2f9eb4f7ee5d3a602fe8e5e
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-09-08 14:38:15 +0100

    revert files to previous commit

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,29 +4,13 @@ module UsersHelper
     obj.present? ? obj.field_value : nil
   end
 
-  def select_delegation_questionnaire(form, user, user_delegate=nil)
+  def select_delegation_questionnaire(form, user)
     if user.available_questionnaires.present?
       form.fields_for :delegations, form.object.delegations.build do |f|
-        f.select(
-          :questionnaire_id,
-          user.available_questionnaires.collect{|p| [h(p.title(I18n.locale.to_s)[0,50])+"...", p.id] },
-          { include_blank: t('manage_delegates.select_a_q'), selected: selected_questionnaire_id_for(user, user_delegate) },
-          class: 'select-delegation-questionnaire'
-        )
+        f.select :questionnaire_id, user.available_questionnaires.collect{|p| [h(p.title(I18n.locale.to_s)[0,50])+"...", p.id] }, { include_blank: t('manage_delegates.select_a_q') }, class: 'select-delegation-questionnaire'
       end
     else
       t('delegation_details.no_questionnaires_available')
     end
-  end
-
-  def is_respondent_of?(user_delegate, respondent)
-    user_delegate.delegators.include?(respondent)
-  end
-
-  def selected_questionnaire_id_for(respondent, user_delegate)
-    delegations       = respondent.delegations
-    user_delegate_obj = delegations.map(&:user_delegate).find{|ud| ud.delegate_id == user_delegate.id }
-    delegation        = delegations.find {|d| d.user_delegate == user_delegate_obj }
-    delegation.try(:questionnaire).try(:id)
   end
 end
