3561be609062c65a035ccc293d94e487d25a66cd
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-09-14 15:17:39 +0100

    Add a delegate by selecting existing or creating a new one

--- app/controllers/user_delegates_controller.rb
+++ app/controllers/user_delegates_controller.rb
@@ -16,6 +16,7 @@ class UserDelegatesController < ApplicationController
   def new
     raise CanCan::AccessDenied.new(t('flash_messages.not_authorized')) if !current_user || (!current_user.role?(:admin) && !current_user.role?(:respondent))
     user = User.find(params[:user_id])
+    @delegates = User.delegates
     @user_delegate = user.user_delegates.new
   end
 
@@ -24,29 +25,38 @@ class UserDelegatesController < ApplicationController
     success = false
     @user_delegate = UserDelegate.new(params[:user_delegate])
     @user_delegate.user = current_user
-    delegate = User.find_by_email(params[:delegate_email])
-    role = Role.find_by_name("delegate")
-    if delegate
-      @user_delegate.delegate_id = delegate.id
-      delegate.roles << role unless delegate.roles.include?(role)
-      delegate.save
-      if @user_delegate.save
+    if @user_delegate.delegate_id
+      if UserDelegate.find_by_user_id_and_delegate_id(@user_delegate.user_id, @user_delegate.delegate_id)
+        flash[:error] = t('user_delegates.already_a_delegate')
+      elsif @user_delegate.save
         UserMailer.delegate_request(@user_delegate).deliver
-        success = true
-      elsif UserDelegate.find_by_user_id_and_delegate_id(@user_delegate.user_id, @user_delegate.delegate_id)
-        flash[:error] = "The user with that email is already in your list of delegates."
-        success = true
       end
+      success = true
     else
-      password = SecureRandom.hex.first(8)
-      delegate = User.new(:email => params[:delegate_email], :password => password, :password_confirmation => password,
-      :language => (params[:delegate][:language] || "en"), :first_name => params[:delegate_first_name], :last_name => params[:delegate_last_name])
-      delegate.roles << role
-      if delegate.save
+      delegate = User.find_by_email(params[:delegate_email])
+      role = Role.find_by_name("delegate")
+      if delegate
         @user_delegate.delegate_id = delegate.id
+        delegate.roles << role unless delegate.roles.include?(role)
+        delegate.save
         if @user_delegate.save
-          UserMailer.delegate_registration(delegate, password, "http://#{request.host}/").deliver
+          UserMailer.delegate_request(@user_delegate).deliver
           success = true
+        elsif UserDelegate.find_by_user_id_and_delegate_id(@user_delegate.user_id, @user_delegate.delegate_id)
+          flash[:error] = t('user_delegates.email_already_a_delegate')
+          success = true
+        end
+      else
+        password = SecureRandom.hex.first(8)
+        delegate = User.new(:email => params[:delegate_email], :password => password, :password_confirmation => password,
+        :language => (params[:delegate][:language] || "en"), :first_name => params[:delegate_first_name], :last_name => params[:delegate_last_name])
+        delegate.roles << role
+        if delegate.save
+          @user_delegate.delegate_id = delegate.id
+          if @user_delegate.save
+            UserMailer.delegate_registration(delegate, password, "http://#{request.host}/").deliver
+            success = true
+          end
         end
       end
     end
