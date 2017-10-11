8cd3dcce3c005d40fbca4c43a85cccfe2399142c
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-10-11 14:30:32 +0100

    Enable/disable all items in question when clicking on mark as answered

--- app/assets/javascripts/respondent/question_answered.js
+++ app/assets/javascripts/respondent/question_answered.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
   $(document).on('change', '.question-answered-box', function(){
+    the_id = $(this).data('the-id')
     text_answer_field = $(this).closest('.text-answer').find('.text-answer-field')
     if($(this).prop('checked')) {
       $(text_answer_field).attr("readonly", true)
       $(text_answer_field).addClass("disabled")
+      $("input[name='answers["+the_id+"]']:radio").attr("disabled", true)
+      $("li.answer-option-"+the_id+" textarea").attr("disabled", true)
     }
     else {
       $(text_answer_field).attr("disabled", false)
       $(text_answer_field).attr("readonly", false)
       $(text_answer_field).removeClass("disabled")
+      $("input[name='answers["+the_id+"]']:radio").attr("disabled", false)
+      $("li.answer-option-"+the_id+" textarea").attr("disabled", false)
       $('.sticky_save_all').click();
     }
   });
