4eeccb16eece6f25694a989797f527f2523b9fb0
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-10-26 10:13:40 +0100

    Mark as answered to disable also matrix dropdowns

--- app/assets/javascripts/respondent/question_answered.js
+++ app/assets/javascripts/respondent/question_answered.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
   $(document).on('change', '.question-answered-box', function(){
     the_id = $(this).data('the-id')
     text_answer_field = $(this).closest('.text-answer').find('.text-answer-field')
+    matrix_answer_field = $(this).closest('.answer_fields_wrapper').find('.submission-matrix')
     if($(this).prop('checked')) {
       $(text_answer_field).attr("readonly", true)
       $(text_answer_field).addClass("disabled")
       $("input[name='answers["+the_id+"]']:radio").attr("disabled", true)
       $("li.answer-option-"+the_id+" textarea").attr("disabled", true)
       $("select#answers_"+the_id).attr("disabled", true)
+      $(matrix_answer_field).find('select').attr("disabled", true)
     }
     else {
       $(text_answer_field).attr("disabled", false)
@@ -16,6 +18,7 @@ $(document).ready(function() {
       $("input[name='answers["+the_id+"]']:radio").attr("disabled", false)
       $("li.answer-option-"+the_id+" textarea").attr("disabled", false)
       $("select#answers_"+the_id).attr("disabled", false)
+      $(matrix_answer_field).find('select').attr("disabled", false)
       $('.sticky_save_all').click();
     }
   });
