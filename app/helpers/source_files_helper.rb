467ca0d67715e46e2d4d95309f4446a79498cf8b
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-11-28 18:27:27 +0000

    Fix broken link in source files page

--- app/helpers/source_files_helper.rb
+++ app/helpers/source_files_helper.rb
@@ -6,7 +6,7 @@ module SourceFilesHelper
       when 1
         "File successfully parsed"
       when 2
-        "There were some errors when parsing this file. #{link_to "Details", edit_source_file_path(source_file.id)}"
+        "There were some errors when parsing this file. #{link_to "Details", edit_source_file_path(source_file.id)}".html_safe
     end
   end
 end
