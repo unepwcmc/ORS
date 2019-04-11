13a6b4516a382803099b01b2014c6eeb639e7245
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-04-11 10:31:40 +0100

    Fetch original document if document from cloned questionnaire doesn't exist
    (cherry picked from commit 047408013dacf57152032386fee67d3837fd550e)

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -4,6 +4,10 @@ class DocumentsController < ApplicationController
     answer = Answer.find(params[:answer_id], :include => :questionnaire)
     raise CanCan::AccessDenied.new("You are not authorized to download this document", :show, Document) if answer.questionnaire.private_documents && !current_user # answer.user != current_user || answer.last_editor != current_user
     document = answer.documents.find(params[:id])
+    # If document doesn't exist in filesystem, this probably means
+    # the questionnaire has been cloned from another one and here there's
+    # still the original file to be fetched.
+    document = document.original unless File.exists?(document.doc.path)
     send_file document.doc.path, :type => document.doc_content_type
   end
 end
