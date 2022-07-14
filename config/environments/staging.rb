c92a53491107bdb65b2be8435b7d78050957f22d
roshan-mathewtech <roshan.mathewtech@gmail.com>
2022-07-14 14:48:43 +0100

    Change mail delivery method from smtp to sendmail

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -52,7 +52,7 @@ OnlineReportingTool::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.asset_host = secrets['asset_host']
   config.action_mailer.default_url_options = {:host => secrets['host']}
   config.action_mailer.smtp_settings = {
